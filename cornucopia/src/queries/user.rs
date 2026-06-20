4f9d3557469e1ec617e75928d2325f92eb228bcd
ce1r <285079711+ce1r@users.noreply.github.com>
2026-06-20 15:00:58 +0300

    Add moderator request

--- cornucopia/src/queries/user.rs
+++ cornucopia/src/queries/user.rs
@@ -49,6 +49,11 @@ pub struct SaveStatsParams<T1: crate::StringSql> {
     pub user_id: i32,
     pub gjp2: T1,
 }
+#[derive(Debug)]
+pub struct GetModLevelParams<T1: crate::StringSql> {
+    pub user_id: i32,
+    pub gjp2: T1,
+}
 #[derive(Debug, Clone, PartialEq)]
 pub struct GetUser {
     pub id: i32,
@@ -415,6 +420,70 @@ where
 {
     pub fn map<R>(self, mapper: fn(&str) -> R) -> StringQuery<'c, 'a, 's, C, R, N> {
         StringQuery {
+            client: self.client,
+            params: self.params,
+            query: self.query,
+            cached: self.cached,
+            extractor: self.extractor,
+            mapper,
+        }
+    }
+    pub async fn one(self) -> Result<T, tokio_postgres::Error> {
+        let row =
+            crate::client::async_::one(self.client, self.query, &self.params, self.cached).await?;
+        Ok((self.mapper)((self.extractor)(&row)?))
+    }
+    pub async fn all(self) -> Result<Vec<T>, tokio_postgres::Error> {
+        self.iter().await?.try_collect().await
+    }
+    pub async fn opt(self) -> Result<Option<T>, tokio_postgres::Error> {
+        let opt_row =
+            crate::client::async_::opt(self.client, self.query, &self.params, self.cached).await?;
+        Ok(opt_row
+            .map(|row| {
+                let extracted = (self.extractor)(&row)?;
+                Ok((self.mapper)(extracted))
+            })
+            .transpose()?)
+    }
+    pub async fn iter(
+        self,
+    ) -> Result<
+        impl futures::Stream<Item = Result<T, tokio_postgres::Error>> + 'c,
+        tokio_postgres::Error,
+    > {
+        let stream = crate::client::async_::raw(
+            self.client,
+            self.query,
+            crate::slice_iter(&self.params),
+            self.cached,
+        )
+        .await?;
+        let mapped = stream
+            .map(move |res| {
+                res.and_then(|row| {
+                    let extracted = (self.extractor)(&row)?;
+                    Ok((self.mapper)(extracted))
+                })
+            })
+            .into_stream();
+        Ok(mapped)
+    }
+}
+pub struct I16Query<'c, 'a, 's, C: GenericClient, T, const N: usize> {
+    client: &'c C,
+    params: [&'a (dyn postgres_types::ToSql + Sync); N],
+    query: &'static str,
+    cached: Option<&'s tokio_postgres::Statement>,
+    extractor: fn(&tokio_postgres::Row) -> Result<i16, tokio_postgres::Error>,
+    mapper: fn(i16) -> T,
+}
+impl<'c, 'a, 's, C, T: 'c, const N: usize> I16Query<'c, 'a, 's, C, T, N>
+where
+    C: GenericClient,
+{
+    pub fn map<R>(self, mapper: fn(i16) -> R) -> I16Query<'c, 'a, 's, C, R, N> {
+        I16Query {
             client: self.client,
             params: self.params,
             query: self.query,
@@ -921,5 +990,54 @@ impl<'c, 'a, 's, C: GenericClient, T1: crate::StringSql>
             &params.user_id,
             &params.gjp2,
         )
+    }
+}
+pub struct GetModLevelStmt(&'static str, Option<tokio_postgres::Statement>);
+pub fn get_mod_level() -> GetModLevelStmt {
+    GetModLevelStmt(
+        "SELECT mod_level FROM users WHERE id = $1 AND gjp2 = $2",
+        None,
+    )
+}
+impl GetModLevelStmt {
+    pub async fn prepare<'a, C: GenericClient>(
+        mut self,
+        client: &'a C,
+    ) -> Result<Self, tokio_postgres::Error> {
+        self.1 = Some(client.prepare(self.0).await?);
+        Ok(self)
+    }
+    pub fn bind<'c, 'a, 's, C: GenericClient, T1: crate::StringSql>(
+        &'s self,
+        client: &'c C,
+        user_id: &'a i32,
+        gjp2: &'a T1,
+    ) -> I16Query<'c, 'a, 's, C, i16, 2> {
+        I16Query {
+            client,
+            params: [user_id, gjp2],
+            query: self.0,
+            cached: self.1.as_ref(),
+            extractor: |row| Ok(row.try_get(0)?),
+            mapper: |it| it,
+        }
+    }
+}
+impl<'c, 'a, 's, C: GenericClient, T1: crate::StringSql>
+    crate::client::async_::Params<
+        'c,
+        'a,
+        's,
+        GetModLevelParams<T1>,
+        I16Query<'c, 'a, 's, C, i16, 2>,
+        C,
+    > for GetModLevelStmt
+{
+    fn params(
+        &'s self,
+        client: &'c C,
+        params: &'a GetModLevelParams<T1>,
+    ) -> I16Query<'c, 'a, 's, C, i16, 2> {
+        self.bind(client, &params.user_id, &params.gjp2)
     }
 }
