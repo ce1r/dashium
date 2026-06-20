8fcd11674ac66d38318e733738d096074a42a33f
ce1r <285079711+ce1r@users.noreply.github.com>
2026-06-21 02:21:38 +0300

    Add user unblocking

--- cornucopia/src/queries/social.rs
+++ cornucopia/src/queries/social.rs
@@ -6,6 +6,12 @@ pub struct BlockUserParams<T1: crate::StringSql> {
     pub target_id: i32,
     pub gjp2: T1,
 }
+#[derive(Debug)]
+pub struct UnblockUserParams<T1: crate::StringSql> {
+    pub user_id: i32,
+    pub target_id: i32,
+    pub gjp2: T1,
+}
 use crate::client::async_::GenericClient;
 use futures::{self, StreamExt, TryStreamExt};
 pub struct BlockUserStmt(&'static str, Option<tokio_postgres::Statement>);
@@ -49,6 +55,53 @@ impl<'a, C: GenericClient + Send + Sync, T1: crate::StringSql>
         &'a self,
         client: &'a C,
         params: &'a BlockUserParams<T1>,
+    ) -> std::pin::Pin<
+        Box<dyn futures::Future<Output = Result<u64, tokio_postgres::Error>> + Send + 'a>,
+    > {
+        Box::pin(self.bind(client, &params.user_id, &params.target_id, &params.gjp2))
+    }
+}
+pub struct UnblockUserStmt(&'static str, Option<tokio_postgres::Statement>);
+pub fn unblock_user() -> UnblockUserStmt {
+    UnblockUserStmt(
+        "DELETE FROM blocks USING users WHERE blocks.user_id = users.id AND blocks.user_id = $1 AND blocks.target_id = $2 AND users.gjp2 = $3",
+        None,
+    )
+}
+impl UnblockUserStmt {
+    pub async fn prepare<'a, C: GenericClient>(
+        mut self,
+        client: &'a C,
+    ) -> Result<Self, tokio_postgres::Error> {
+        self.1 = Some(client.prepare(self.0).await?);
+        Ok(self)
+    }
+    pub async fn bind<'c, 'a, 's, C: GenericClient, T1: crate::StringSql>(
+        &'s self,
+        client: &'c C,
+        user_id: &'a i32,
+        target_id: &'a i32,
+        gjp2: &'a T1,
+    ) -> Result<u64, tokio_postgres::Error> {
+        client.execute(self.0, &[user_id, target_id, gjp2]).await
+    }
+}
+impl<'a, C: GenericClient + Send + Sync, T1: crate::StringSql>
+    crate::client::async_::Params<
+        'a,
+        'a,
+        'a,
+        UnblockUserParams<T1>,
+        std::pin::Pin<
+            Box<dyn futures::Future<Output = Result<u64, tokio_postgres::Error>> + Send + 'a>,
+        >,
+        C,
+    > for UnblockUserStmt
+{
+    fn params(
+        &'a self,
+        client: &'a C,
+        params: &'a UnblockUserParams<T1>,
     ) -> std::pin::Pin<
         Box<dyn futures::Future<Output = Result<u64, tokio_postgres::Error>> + Send + 'a>,
     > {
