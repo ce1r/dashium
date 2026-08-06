8257995210811a2dbc8dfe46da6a76cc2558423a
ce1r <285079711+ce1r@users.noreply.github.com>
2026-08-06 14:27:26 +0300

    Split user data and saved levels

--- src/user/backupGJAccountNew.rs
+++ src/user/backupGJAccountNew.rs
@@ -2,6 +2,8 @@ use crate::Database;
 use crate::Result;
 use crate::util::verify_gjp2;
 use axum_extra::extract::Form;
+use base64::Engine;
+use base64::engine::general_purpose::URL_SAFE;
 use chacha20poly1305::ChaCha20Poly1305;
 use chacha20poly1305::KeyInit;
 use chacha20poly1305::Nonce;
@@ -25,15 +27,26 @@ pub async fn backupGJAccountNew(Form(form): Form<Data>) -> Result<String> {
         .try_into()
         .unwrap_or_default();
 
+    let (user_data, user_levels) = form
+        .saveData
+        .split_once(';')
+        .ok_or_else(|| anyhow::anyhow!(""))?;
+
+    let user_data = URL_SAFE.decode(user_data)?;
+    let user_levels = URL_SAFE.decode(user_levels)?;
+
     let key = Key::<ChaCha20Poly1305>::from(hash);
     let cipher = ChaCha20Poly1305::new(&key);
     let nonce = Nonce::from([0u8; 12]);
 
-    let ciphertext = cipher.encrypt(&nonce, form.saveData.as_ref())?;
+    let ciphertext = cipher.encrypt(&nonce, user_data.as_ref())?;
 
-    let path = format!("data/users/{}.user", form.accountID);
-    let mut file = File::create(path).await?;
-    file.write_all(&ciphertext).await?;
+    let path1 = format!("data/users/{}.userdata", form.accountID);
+    let path2 = format!("data/users/{}.userlevels", form.accountID);
+    let mut file1 = File::create(path1).await?;
+    let mut file2 = File::create(path2).await?;
+    file1.write_all(&ciphertext).await?;
+    file2.write_all(&user_levels).await?;
 
     Ok("1".to_string())
 }
--- src/user/syncGJAccountNew.rs
+++ src/user/syncGJAccountNew.rs
@@ -2,6 +2,8 @@ use crate::Database;
 use crate::Result;
 use crate::util::verify_gjp2;
 use axum_extra::extract::Form;
+use base64::Engine;
+use base64::engine::general_purpose::URL_SAFE;
 use chacha20poly1305::ChaCha20Poly1305;
 use chacha20poly1305::Key;
 use chacha20poly1305::KeyInit;
@@ -23,14 +25,20 @@ pub async fn syncGJAccountNew(Form(form): Form<Data>) -> Result<String> {
         .try_into()
         .unwrap_or_default();
 
-    let path = format!("data/users/{}.user", form.accountID);
-    let save_data = fs::read(path).await?;
+    let path1 = format!("data/users/{}.userdata", form.accountID);
+    let path2 = format!("data/users/{}.userlevels", form.accountID);
+    let user_data = fs::read(path1).await?;
+    let user_levels = fs::read(path2).await?;
 
     let key = Key::from(hash);
     let cipher = ChaCha20Poly1305::new(&key);
     let nonce = Nonce::from([0u8; 12]);
 
-    let ciphertext = cipher.decrypt(&nonce, save_data.as_ref())?;
+    let ciphertext = cipher.decrypt(&nonce, user_data.as_ref())?;
 
-    Ok(format!("{};21;30;a;a", String::from_utf8(ciphertext)?))
+    Ok(format!(
+        "{};{};21;30;a;a",
+        URL_SAFE.encode(ciphertext),
+        URL_SAFE.encode(user_levels)
+    ))
 }
